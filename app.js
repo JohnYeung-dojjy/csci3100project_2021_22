24fcc60b26576b36e8acbc1d9ed6c17e47609f66
Yeung Ching Fung <p80246@gmail.com>
2022-02-20 21:54:41 +0800

    added app.js for display usage

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.get('/', (req, res) => {
+    // send the file 'index.html. in the folder of the current script
+    res.sendFile(__dirname + '/login/login.html');
+});
+
+app.get('/game', (req, res) => {
+    res.sendFile(__dirname + '/game/game.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login/login.html');
+});
+
+const server = app.listen(3000);
