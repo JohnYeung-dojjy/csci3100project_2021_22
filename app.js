bff5f973980dc05a1d3527b1cb3abd481acccd3f
littlebrian123 <Q1034288092@gmail.com>
2022-03-04 16:00:03 +0800

    new edit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const ejs = require('ejs');
 const database = require('./models/testdb');
 const system = require('./models/System_functions');
+const { render } = require('express/lib/response');
 app.use(bodyParser.urlencoded({ type: 'application/x-www-form-urlencoded', extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use('/static', express.static(__dirname + '/public'));
@@ -35,17 +36,34 @@ app.get('/game', (req, res) => {
     res.sendFile(__dirname + '/pages/game/game.html');
 })
 
-app.get('/sample', (req, res) => {
+
+/* app.get('/sample', (req, res) => {
     res.render('sample.ejs', {
         title: 'Homepage',
         users: ['BRIan', 'TOM', 'JErrY']
     });
 
+}) */
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/pages/admin/admin.html');
 })
 
-app.post('/loginverify', (req, res) => {
-    console.log(req.body);
-    res.end();
+app.post('/loginverify', async (req, res) => {
+    /* console.log(req.body.username);//req.body is already a object */
+    await system.loginAccount(req.body).then((code) => {
+        console.log(code);
+        if (code === 11100) {
+            res.render('login.ejs', {
+                errormessage: 'the username or password is wrong!',
+                style: 'inset 0 3px 5px rgba(255, 0, 0, 0.5), 0 4px 0px rgba(255, 0, 0, 0.45)'
+            })
+        }
+        else {
+            res.redirect('/admin');
+        }
+    });
+
 })
 
 
@@ -58,7 +76,8 @@ app.post('/regverify', (req, res) => {
         obj = JSON.parse(data);//from json to object
         /* console.log(data); */
         await system.registerNewAccount(obj).then((code) => {
-            /*            let body = JSON.stringify({ code: code }); */
+            console.log(code);
+            /* let body = JSON.stringify({ code: code }); */
             res.send({ code: code });//automatically change to json
         })
     })
--- models/System_functions.js
+++ models/System_functions.js
@@ -9,6 +9,7 @@ const Map = require("./map");
 const Leaderboard = require("./leader_board");
 const Feedback = require("./feedback");
 const { countDocuments } = require("./User");
+const { get } = require("jquery");
 
 module.exports = { registerNewAccount, loginAccount, forgetPassword, forgetUserName };
 
@@ -21,37 +22,46 @@ async function registerNewAccount(obj) {
             admin: obj.admin
         });
         let state = await instance.save()
-            .then(() => {//normally it will return the instance, but code is easily to recognize
+            .then((acc) => {
                 console.log('User Saved Successfully!');
-                console.log(instance);
-                return 0;
+                console.log(typeof acc.id);//string;
+                return acc.id;
             })
             .catch(
                 (err) => {
                     console.log(err);
-                    return err.code;
+                    return err.code;//number
                 }
             )
-        return state;//if 0, it is success, if duplicate,the code is 11000
+        return state;// if duplicate,the code is 11000,defined by mongodb
     } catch (e) {
         console.log(e.message);
         return -1;
     }
 }
 
-async function loginAccount(username, password) {
+async function loginAccount(obj) {
     try {
-        const acc = await User.find({ username: username, password: password });
-        console.log(acc);
-        return acc;
+        let code = await User.findOne({ username: obj.username, password: obj.password }).lean().then((acc) => {
+            if (acc !== null) {
+                return acc._id.toString();
+            }
+            else {
+                return 11100;
+            }
+        }).catch((err) => {
+            console.log(err);
+            return err.code;
+        })
+        return code;
     } catch (err) {
         console.log(err.message);
+        return -1;
     }
-
 }
 
 async function forgetUserName(user_email) {
-    //Login with Username or Email or both?
+    //Maybe we don't need this?I prefer that people can only use unique username and email to retrive their password.
 
 }
 
