c49d420f5faf1aecf2f44ce32975fe4d2f34547a
littlebrian123 <Q1034288092@gmail.com>
2022-03-18 17:00:47 +0800

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/login/login.html');
+    res.sendFile('/pages/login/login.html');
 })
 const server = app.listen(3000);
 module.exports = app;
