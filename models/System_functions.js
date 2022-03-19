1473cfd3f5ac9b1a2b30459470970ca30a6f5df8
littlebrian123 <Q1034288092@gmail.com>
2022-03-19 14:22:24 +0800

    modified database function, easier for Patrick to follow

--- models/System_functions.js
+++ models/System_functions.js
@@ -11,8 +11,14 @@ const Feedback = require("./feedback");
 const { countDocuments } = require("./User");
 const { get } = require("jquery");
 
-module.exports = { registerNewAccount, loginAccount, forgetPassword, forgetUserName };
+module.exports = { registerNewAccount, loginAccount, forgetPassword };
 
+/*
+the following function demostrates a save() for database creation and update
+again, you most likely modify only instance every time.
+with this implementation, duplicate key situation will caught, and -1 will be returned
+ I don't need you return me a specific data but only ensure that the whole data type is  'object'
+*/
 async function registerNewAccount(obj) {
     try {
         const instance = new User({
@@ -21,57 +27,90 @@ async function registerNewAccount(obj) {
             user_email: obj.user_email,
             admin: obj.admin
         });
-        let state = await instance.save()
-            .then((acc) => {
-                console.log('User Saved Successfully!');
-                console.log(typeof acc.id);//string;
-                return acc;
-            })
-            .catch(
-                (err) => {
-                    console.log(err);
-                    return err.code;//number
-                }
-            )
-        return state;// if duplicate,the code is 11000,defined by mongodb
+        let content = await instance.save().then(
+            (result) => {
+                return result.toObject();
+            }
+        )
+        return content;
     } catch (e) {
         console.log(e.message);
         return -1;
     }
 }
 
+
+// the following demostrates find() method, if you use find() with lean(), it will return you an array of object.
 async function loginAccount(obj) {
     try {
-        let code = await User.findOne({ username: obj.username, password: obj.password }).lean().then((acc) => {
-            if (acc !== null) {
-                return acc;
+        let query = User.find({ username: obj.username, password: obj.password }).lean().exec();
+        //lean() can only be used in findone() and find(), it has no use with save();
+        let content = query.then(
+            (result) => {
+                if (result === null || result.length === 0) {
+                    return 11100;
+                }
+                else {
+                    return result[0];
+                }
             }
-            else {
-                return 11100;
-            }
-        }).catch((err) => {
-            console.log(err);
-            return err.code;
-        })
-        return code;
-    } catch (err) {
+        )
+        return content;
+    }
+    catch (err) {
         console.log(err.message);
         return -1;
     }
 }
 
-async function forgetUserName(user_email) {
-    //Maybe we don't need this?I prefer that people can only use unique username and email to retrive their password.
 
-}
-
-async function forgetPassword(_id) {
+/*
+This method draw a template for you with findOne() function,
+I deliberately separate the query and arrow function,so most of the time you only need to modify the query part
+*/
+/* async function loginAccount(obj) {
     try {
-        const email = await User.find({ _id: _id }).select("user_email");
-        console.log(email);
-        //send email, new password mechanism??
-    } catch (err) {
+        let query = User.findOne({ username: obj.username, password: obj.password }).lean().exec();
+        //By using lean(), the server can directly get an object which is more convenient to use.
+       //lean() can only be used in findone() and find(), it has no use with save();
+        let content = query.then(
+            (result) => {
+                if (result === null || result.length === 0) {
+                    return 11100;
+                }
+                else {
+                    console.log(result);
+                    return result;
+                }
+            }
+        )
+        return content;
+    }
+    catch (err) {
         console.log(err.message);
+        return -1;
+    }
+} */
+
+
+async function forgetPassword(obj) {
+    try {
+        let query = User.findOne({ username: obj.username }).lean().exec();
+        let content = query.then(
+            (result) => {
+                if (result === null || result.length === 0) {
+                    return 11100;
+                }
+                else {
+                    return result;
+                }
+            }
+        )
+        return content;
+    }
+    catch (err) {
+        console.log(err.message);
+        return -1;
     }
 }
 
