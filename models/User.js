04499cca5ca838bcad48c6430de46c02a1993fd0
patrillicit <patrillicit@outlook.com>
2022-04-02 23:34:43 +0800

    changed User Icon Data Type

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const express = require("express");
 var UserSchema = mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	user_icon: { type: Buffer },
+	user_icon: { type: File },
 	user_email: { type: String, required: true },
 	admin: { type: Boolean, required: true }
 });
