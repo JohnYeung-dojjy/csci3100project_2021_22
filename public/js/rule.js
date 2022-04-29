74a4b39ccbc46f788a9cffb5c850840878ad5767
John Yeung <cfyeung2357@gmail.com>
2022-04-29 21:12:00 +0800

    added header comments in lboard.js and rule.js

--- public/js/rule.js
+++ public/js/rule.js
@@ -1,3 +1,14 @@
+/**
+ * Game: This file contains functions that handles all operations that can be done when rules are displayed.
+ * 
+ * Author: YEUNG Ching Fung
+ * 
+ * Version 1: Written 8 April 2022
+ * 
+ * functions: 
+ *    button.onclick : display the game visuals, this button is only available when the camera input is ready 
+ */
+
 // game variables
 let is_game_start = false;
 
